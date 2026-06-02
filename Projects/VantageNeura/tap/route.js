a97b261db75590f42cc25e9249882c439b03acbe
promptlane25-stack <promptlane25@gmail.com>
2026-06-02 15:34:56 +0100

    Add client routes for pizza shop and karate club

---
+++ Projects/VantageNeura/tap/route.js
@@ -0,0 +1,4 @@
+const clientRoutes = {
+    "pizza-shop": "https://search.google.com/local/writereview?placeid=YOUR_PIZZA_ID",
+    "NorthgateKarateClub": "https://g.page/r/CfOneybtkNP1EBM/review"
+};
