f23acad608dae4b4f100bb4c5177b0ba0cc20c39
promptlane25-stack <promptlane25@gmail.com>
2026-06-05 16:49:49 +0100

    Update routes.js

--- Projects/VantageNeura/tap/routes.js
+++ Projects/VantageNeura/tap/routes.js
@@ -1,4 +1,9 @@
 const clientRoutes = {
     "pizza-shop": "https://search.google.com/local/writereview?placeid=YOUR_PIZZA_ID",
     "NorthgateKarateClub": "https://g.page/r/CfOneybtkNP1EBM/review"
+ 
+        // AA Motor Medic Tracking Keys
+    "AAMotorMedicReviewNFC": "https://g.page/r/CRs8KFzbGS7BEBM/review?utm_source=nfcCard&utm_medium=automation",
+    "AAMotorMedicReviewSMS": "https://g.page/r/CRs8KFzbGS7BEBM/review?utm_source=sms&utm_medium=automation",
+    "AAMotorMedicReviewEmail": "https://g.page/r/CRs8KFzbGS7BEBM/review?utm_source=email&utm_medium=automation"
 };
